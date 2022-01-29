961ed83df7d5a39faab0c905c97ac2f9e6f79d11
vitrym <vitrym@debian>
2022-01-29 15:49:19 +0100

    Chat webui fonctionnel

--- src/components/Lechat.vue
+++ src/components/Lechat.vue
@@ -2,18 +2,20 @@
   <table id="liste-chat">
     <thead>
       <tr>
-        <th>N°</th>
-        <th>Description</th>
+        <th>N° <td>{{ chatReponse.canalId }}</td></th>
+        <th>Description <td>{{ chatReponse.canalName }}</td></th>
       </tr>
     </thead>
-    <tbody>
+    <tbody v-if="this.chatReponse.canalId == ''">
       <tr v-for="row in this.leschat" :key="row">
         <td>{{ row.canalId }}</td>
         <td>{{ row.canalName }}</td>
       </tr>
     </tbody>
   </table>
-  <button @click="chatConnect">Blablabla ...</button>
+  <button @click="chatConnect(1)">Canal 1</button>
+  <button @click="chatConnect(2)">Canal 2</button>
+  <button @click="chatConnect(3)">Canal 3</button>
   <div>
     <span><pre>{{ chatAllMessage }}</pre></span>
   </div>
@@ -28,8 +30,9 @@ import axios from "axios";
 export default {
   data() {
     return {
+
     ws : null,
-    serverUrl: "ws://localhost:8085/ws/multichat/1:Mikky",
+    serverUrl: "",
       leschat: {
         canalId: "",
         canalName: "",
@@ -41,45 +44,63 @@ export default {
         leContenu: "",
       },
       chatReponse: {
-        canalId: 0,
+        canalId: "",
         lePseudo: "",
         leContenu: "",
+        canalName: "",
       },
       chatAllMessage: "",
     };
   },
   mounted() {
     console.log("Appel service Rest ...")
-    axios.get("http://localhost:8085/services/multichat/1").then((response) => {
+    axios.get("http://localhost:8080/services/multichat/").then((response) => {
       this.leschat = response.data;
       console.log(this.leschat)
     });
   },
   methods: {
     handleNewMessage(event) {
+      
         console.log(event.data);
         this.chatMessage = JSON.parse(event.data);
-        this.chatAllMessage = this.chatAllMessage + this.chatMessage.lePseudo + " : " + this.chatMessage.leContenu + "\n";
+        
+        if(this.chatMessage.lePseudo != this.chatReponse.lePseudo & this.chatMessage.canalId == this.chatMessage.canalId) {
+          this.chatAllMessage = this.chatAllMessage + this.chatMessage.lePseudo + " : " + this.chatMessage.leContenu + "\n";
+        }
+        
     },
     chatBye(){
+        console.log("On est déconnecté !!!");
         this.ws.close();
         this.ws = null;
         this.chatAllMessage='';
+        this.chatReponse.canalId = "";
+        this.chatReponse.canalName = "";
+        this.serverUrl = "";
+
     },
     sendReponse(){
+      
       this.chatReponse.lePseudo = this.$store.getters.getLeUser;
       this.chatReponse.leContenu = this.reponse; 
       this.ws.send(JSON.stringify(this.chatReponse)); 
       this.reponse = '';
     },
-    chatConnect() {     
+    chatConnect(i) {     
+      if (this.serverUrl != "") {
+        this.chatBye(); 
+      }
       console.log("Starting connection to WebSocket Server");
+      this.serverUrl = "ws://localhost:8080/ws/multichat/" + i + ":" + this.$store.getters.getLeUser;
       this.ws = new WebSocket( this.serverUrl );
       this.ws.onopen = function (event) {
         console.log(event);
         console.log("On est connecté !!!");
       };
       this.ws.addEventListener('message', (event) => { this.handleNewMessage(event) });
+      this.chatReponse.canalId = i;
+      this.chatReponse.canalName = this.leschat[i-1].canalName;
     }
   },
 };
